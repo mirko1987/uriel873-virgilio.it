e45c397a2405b7c82de4753fb33554070c9b39a8
mirko uriel <mirkouriel@MacBook-Pro-di-mirko.local>
2020-03-29 04:11:03 +0200

    pomodoro start

---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent implements OnInit {
+
+
+ minutes: number;
+  seconds: number;
+  isPaused: boolean;
+  buttonLabel: string;
+
+  constructor() {
+    this.resetTimer();
+    setInterval(() => this.tick(), 1000);
+  }
+
+  ngOnInit(){}
+
+  resetTimer(): void {
+    this.isPaused = true;
+    this.minutes = 24;
+    this.seconds = 59;
+    this.buttonLabel = 'Start';
+  }
+
+  private tick(): void {
+    if (!this.isPaused) {
+      this.buttonLabel = 'Pause';
+
+      if (--this.seconds < 0) {
+        this.seconds = 59;
+        if (--this.minutes < 0) {
+          this.resetTimer();
+        }
+      }
+    }
+  }
+
+  togglePause(): void {
+    this.isPaused = !this.isPaused;
+    if (this.minutes < 24 || this.seconds < 59) {
+      this.buttonLabel = this.isPaused ? 'Resume' : 'Pause';
+    }
+  }
+}
+
+
